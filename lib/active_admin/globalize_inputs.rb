23084f0c29a578431150d91096bc04de48e7c570
Pol Rodriguez <pol@lumoslabs.com>
2017-06-05 10:35:10 -0700

    Remove reference to form_buffers (#2)
    * Remove deprecated `form_buffers`
    
    * Make it one

--- lib/active_admin/globalize_inputs.rb
+++
@@ -1,8 +0,0 @@
-module ActiveAdmin
-  class FormBuilder
-    def globalize_inputs(*args, &proc)
-      content = with_new_form_buffer { super }
-      form_buffers.last << content.html_safe
-    end
-  end
-end
--- lib/activeadmin_globalize_inputs.rb
+++ lib/activeadmin_globalize_inputs.rb
@@ -1,2 +1,1 @@
 require 'formtastic/globalize_inputs'
-require 'active_admin/globalize_inputs'
